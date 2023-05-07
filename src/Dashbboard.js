eb57577f1c9d9df2cbb3da6959ae5f49c10829a1
SinghVivek2023 <113018935+SinghVivek2023@users.noreply.github.com>
2023-05-07 15:46:19 +0530

    updated dashboard as all components in one place

--- src/Dashbboard.js
+++ src/Dashbboard.js
@@ -1,7 +1,22 @@
 import React from "react";
 
 function Dashbboard() {
-  return <div>Dashbboard</div>;
+  return (
+    <>
+      <header>
+        <a href="#">Logo</a>
+        <nav>
+          <ul>
+            <li>Dashbboard</li>
+            <li>Client Customization Form</li>
+            <li>Sales Team Dashboard</li>
+            <li>Support Team Dashboard</li>
+            <li>Technical Team Dashboard</li>
+          </ul>
+        </nav>
+      </header>
+    </>
+  );
 }
 
 export default Dashbboard;
