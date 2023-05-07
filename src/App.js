4963e8012f50f3ce08d1696230bcd0bdb18ac94f
SinghVivek2023 <113018935+SinghVivek2023@users.noreply.github.com>
2023-05-07 15:02:58 +0530

    create components folder contains ClientCustomizeform.js And update App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
 import Dashbboard from "./Dashbboard";
-
+import ClientCustomizeform from "./components/ClientCustomizeform";
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Dashbboard />} />
+        <Route path="ClientCustomizeform" element={<ClientCustomizeform />} />
       </Routes>
     </BrowserRouter>
   );
