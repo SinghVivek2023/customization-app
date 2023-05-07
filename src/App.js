96bb4c4eecd4cf74ea9590f41f7f273dbba055d5
SinghVivek2023 <113018935+SinghVivek2023@users.noreply.github.com>
2023-05-07 18:58:37 +0530

    removed App.css from App.js and added divider of ChakraUi in clientcustomization.js for testing which not working

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import "./App.css";
 import DashBoard from "./DashBoard";
 import ClientCustomizationForm from "./components/ClientCustomizationForm";
 import SalesTeamDashboard from "./components/SalesTeamDashboard";
--- src/components/ClientCustomizationForm.js
+++ src/components/ClientCustomizationForm.js
@@ -1,8 +1,10 @@
 import React from "react";
+import { Divider } from "@chakra-ui/react";
 
 const ClientCustomizationForm = () => {
   return (
     <>
+      <Divider orientation="horizontal" h="2px" />
       <section>
         <h1>Client Customization Form</h1>
       </section>
