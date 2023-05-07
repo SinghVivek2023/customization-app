e5c9a7bd6f57e32a080f2a4008c2339117b4b768
SinghVivek2023 <113018935+SinghVivek2023@users.noreply.github.com>
2023-05-08 02:34:53 +0530

    update as sidebar

--- src/MainHeader.js
+++ src/MainHeader.js
@@ -1,13 +1,23 @@
 import React from "react";
+import { Grid, GridItem } from "@chakra-ui/react";
 import Header from "./components/Header";
 import { Outlet } from "react-router-dom";
 
 function Dashbboard() {
   return (
-    <>
-      <Header />
+    <Grid templateColumns="repeat(6, 1fr)" bg="gray.50">
+      {/* sidebar */}
+      <GridItem
+        as="aside"
+        colSpan={{ base: 6, lg: 2, xl: 1 }}
+        bg="purple.400"
+        minHeight={{ lg: "100vh" }}
+        p={{ base: "20px", lg: "30px" }}
+      >
+        <Header />
+      </GridItem>
       <Outlet />
-    </>
+    </Grid>
   );
 }
 
